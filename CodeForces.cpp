9ef4810e1d47db43f0d8c4cbdd10cd1be0b7b0c5
Priyansh <priyanshg615@gmail.com>
2022-02-01 00:41:04 +0530

    Coding is sometimes boring

--- CodeForces.cpp
+++ CodeForces.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long LL;
 typedef pair<int, int> pii;
@@ -12,30 +12,35 @@ typedef vector<vl> vvl;
 #define ll long long int
 #define lll unsigned long long
 #define ff first
-#define ss second 
-#define all(v) v.begin(),v.end()
-const int M=1e9+7;
-const int m=1e9+7;
+#define ss second
+#define all(v) v.begin(), v.end()
+const int M = 1e9 + 7;
+const int m = 1e9 + 7;
 
-int moadd(int a, int b) { 
-    return ((a % m) + (b % m)) % m; 
-    }
+int moadd(int a, int b)
+{
+    return ((a % m) + (b % m)) % m;
+}
 
-int mosub(int a, int b) {
-     return ((a % m) - (b % m)) % m; 
-     }
+int mosub(int a, int b)
+{
+    return ((a % m) - (b % m)) % m;
+}
 
-int momul(int a, int b) { 
-    return ((a % m) * (b % m)) % m; 
-    }
+int momul(int a, int b)
+{
+    return ((a % m) * (b % m)) % m;
+}
 
-int modiv(int a, int b) { 
-    return ((a % m) / (b % m)) % m; 
-    }
+int modiv(int a, int b)
+{
+    return ((a % m) / (b % m)) % m;
+}
 
-int lcm(int a, int b) {
-     return a / __gcd(a, b) * b;
-      }
+int lcm(int a, int b)
+{
+    return a / __gcd(a, b) * b;
+}
 
 int min(int a, int b)
 {
@@ -59,32 +64,39 @@ int max(int a, int b)
         return b;
     }
 }
-int bin(int a,int b){
-    int ans=1;
-    while(b){
-        if(b & 1){
-            ans=(ans*1LL*a)%M;
-        }
-        a=(a*1LL*a)%M;
-        b>>=1;
-    }
-    return ans;
-}
-int bin_Mul(long long a,long long b){
-   int ans=0;
-    while(b){
-        if(b & 1){
-            ans=(ans+a)%M;
-        }
-        a=(a+a)%M;
-        b>>=1;
-    }
-    return ans;
-}
-bool isprime(int n){
-    for(int i=2;i<n;i++)
+int bin(int a, int b)
+{
+    int ans = 1;
+    while (b)
     {
-        if(n%i==0)
+        if (b & 1)
+        {
+            ans = (ans * 1LL * a) % M;
+        }
+        a = (a * 1LL * a) % M;
+        b >>= 1;
+    }
+    return ans;
+}
+int bin_Mul(long long a, long long b)
+{
+    int ans = 0;
+    while (b)
+    {
+        if (b & 1)
+        {
+            ans = (ans + a) % M;
+        }
+        a = (a + a) % M;
+        b >>= 1;
+    }
+    return ans;
+}
+bool isprime(int n)
+{
+    for (int i = 2; i < n; i++)
+    {
+        if (n % i == 0)
         {
             return false;
         }
@@ -96,44 +108,66 @@ bool isprime(int n){
 bool vowelOrConsonant(char x)
 {
     if (x == 'a' || x == 'e' || x == 'i' ||
-                   x == 'o' || x == 'u')
+        x == 'o' || x == 'u')
         return true;
     else
         return false;
 }
-
-void solve(){
-
- int n,m;
- cin>>n>>m;
-
- vector<int> v;
-
- for(int i=0;i<n;i++)
- {
-     for(int j=0;j<m;j++)
-     {
-         v.push_back(max({abs(i-n+1)+abs(j-m+1),abs(i-0)+abs(j-0),abs(i-0)+abs(j-m+1),abs(i-n+1)+abs(j-0)}));
-     }
- }
-sort(v.begin(),v.end());
-for(auto i:v)
-cout<<i<<" ";
-cout<<"\n";
+int lis(vector<int> &v)
+{
+    multiset<int> s;
+    for (int i = 0; i < v.size(); i++)
+    {
+        s.insert(v[i]);
+        auto it = s.upper_bound(v[i]);
+        if (it != s.end())
+            s.erase(it);
+    }
+    return s.size();
 }
 
+void solve()
+{
 
+    ll hc, dc, hm, dm;
+    cin >> hc >> dc >> hm >> dm;
 
+    ll k, w, a;
+    cin >> k >> w >> a;
 
-int main(){     
+    for (ll i = 0; i <= k; i++)
+    {
+        ll thc = hc + (a * i);
+        ll tdc = dc + (w * (k - i));
+        ll att1, att2;
+        att1 = hm / tdc;
+        att2 = thc / dm;
+
+        if (hm % tdc != 0)
+            att1++;
+
+        if (thc % dm != 0)
+            att2++;
+
+        if (att1 <=att2)
+        {
+            cout << "YES\n";
+            return;
+        }
+    }
+    cout << "NO\n";
+}
+
+int main()
+{
 
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    ll t;
-    cin>>t;
-    while(t--)
+    int t;
+    cin >> t;
+    while (t--)
     {
-    solve();
+        solve();
     }
 }
--- codechef.cpp
+++ codechef.cpp
@@ -1,85 +1,105 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define ll long long int
 #define lll unsigned long long
 #define ff first
-#define ss second 
-#define all(v) v.begin(),v.end()
+#define ss second
+#define all(v) v.begin(), v.end()
 #define ub upper_bound
-const int M=1e9+7;
-ll a[110];
-int bin(int a,int b){
-    int ans=1;
-    while(b){
-        if(b & 1){
-            ans=(ans*1LL*a)%M;
+const int M = 1e9 + 7;
+
+int bin(int a, int b)
+{
+    int ans = 1;
+    while (b)
+    {
+        if (b & 1)
+        {
+            ans = (ans * 1LL * a) % M;
         }
-        a=(a*1LL*a)%M;
-        b>>=1;
+        a = (a * 1LL * a) % M;
+        b >>= 1;
     }
     return ans;
 }
-int bin_Mul(long long a,long long b){
-   int ans=0;
-    while(b){
-        if(b & 1){
-            ans=(ans+a)%M;
+int bin_Mul(long long a, long long b)
+{
+    int ans = 0;
+    while (b)
+    {
+        if (b & 1)
+        {
+            ans = (ans + a) % M;
         }
-        a=(a+a)%M;
-        b>>=1;
+        a = (a + a) % M;
+        b >>= 1;
     }
     return ans;
 }
-int checkABC(int i,string s){
-    return (s[i]=='a' && s[i+1]=='b' && s[i+2]=='c')==true?1:0;
+int checkABC(int i, string s)
+{
+    return (s[i] == 'a' && s[i + 1] == 'b' && s[i + 2] == 'c') == true ? 1 : 0;
 }
 
 int calc(vector<int> &a)
 {
     multiset<int> s;
-    for(int i=0;i<a.size();i++)
+    for (int i = 0; i < a.size(); i++)
     {
         s.insert(a[i]);
-        auto it=s.upper_bound(a[i]);
-        if(it!=s.end())
-        s.erase(it);
+        auto it = s.upper_bound(a[i]);
+        if (it != s.end())
+            s.erase(it);
     }
     return s.size();
 }
 
-void solve(){
-
-int n,k;
-cin>>n>>k;
-
-int a[n];
-for(int i=0;i<n;i++)
-cin>>a[i];
-
-sort(a,a+n);
-
-int ans=0;
-
-for(int i=0;i<n-k;i++)
+void solve()
 {
-    if(i<n-2*k)
-    ans+=a[i];
-    else
-    ans+=(a[i]/a[i+k]);
+
+    ll n;
+    cin >> n;
+    int mx = -1;
+    int k = -1;
+    ll v[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+        int x = v[i];
+        while (x % 2 == 0)
+            x /= 2;
+        if (mx < x)
+        {
+            mx = x;
+            k = i;
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (!(v[i] & 1) and i != k)
+        {
+            while (v[i] % 2 == 0)
+            {
+                v[k] *= 2;
+                v[i] /= 2;
+            }
+        }
+    }
+
+    ll sum = 0;
+    for (int i = 0; i < n; i++)
+        sum += v[i];
+
+    cout << sum << "\n";
 }
 
-cout<<ans<<"\n";
-
-}
-
-int main(){     
+int main()
+{
 
     int t;
-    cin>>t;
-
-    
-    while(t--)
+    cin >> t;
+    while (t--)
     {
-    solve();
+        solve();
     }
 }
