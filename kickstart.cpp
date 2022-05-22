fdbd7aa57ed01a57ee67414c89e0726da7075e86
Priyansh <priyanshg615@gmail.com>
2022-05-22 18:32:18 +0530

    Disappointment

--- kickstart.cpp
+++ kickstart.cpp
@@ -5,19 +5,85 @@ using namespace std;
 #define ff first
 #define ss second
 #define all(v) v.begin(), v.end()
-const lll N=1e12+1;
+const lll N = 1e12 + 1;
+vector<int> ans;
+
+vector<int> isSubsetSum(int set[], int n, int sum, vector<int> &p)
+{
+  if (sum == 0)
+  {
+    return p;
+  }
+  if (n == 0)
+  {
+    return p;
+  }
+
+  if (set[n - 1] > sum)
+    p=isSubsetSum(set, n - 1, sum, p);
+
+  p=isSubsetSum(set, n - 1, sum, p);
+  p=isSubsetSum(set, n - 1, sum - set[n - 1], p);
+
+  p.push_back(set[n - 1]);
+  return p;
+}
 
 void solve(int t)
 {
 
+  ll n, x, y;
+  cin >> n >> x >> y;
 
-// cout << "Case #" << t << ": "<<ans<<"\n";
+  ll sum = (n * (n + 1)) / 2;
+
+  ll c = 1;
+  ll temp = c * x + c * y;
+  bool f = false;
+
+  if (temp == sum)
+    f = true;
+
+  while (temp < sum)
+  {
+    c++;
+    temp = c * x + c * y;
+    if (temp == sum)
+    {
+      f = true;
+      break;
+    }
+  }
+  if (!f)
+    cout << "Case #" << t << ": "
+         << "IMPOSSIBLE"
+         << "\n";
+
+  else
+  {
+    cout << "Case #" << t << ": "
+         << "POSSIBLE"
+         << "\n";
+
+    ll subsum = c * x;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+      arr[i] = i + 1;
+
+    vector<int> p;
+    isSubsetSum(arr, n, subsum, p);
+
+    cout << p.size() << "\n";
+
+    for (auto x : p)
+      cout << x << " ";
+    cout << "\n";
+  }
 }
 
 int main()
 {
 
-  cout<<S[0];
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
 
